8635f03ccd5910cc14e83dfe6a57c172b0917e7e
CRCx86 <crc32zinov@gmail.com>
2016-09-25 13:32:47 +0300

    /* Дублируем текст Считайте с консоли имя файла В методе main подмените объект System.out написанной вами ридер-оберткой по аналогии с лекцией Ваша ридер-обертка должна дублировать вывод всего текста в файл, имя которого вы считали Вызовите готовый метод printSomething(), воспользуйтесь testString Верните переменной System.out первоначальный поток Закройте поток файла
    Пример вывода на экран:
    it's a text for testing
    
    Пример тела файла:
    it's a text for testing
    */

--- src/com/javarush/test/level19/lesson05/task03/Solution.java
+++ src/com/javarush/test/level19/lesson05/task03/Solution.java
@@ -13,8 +13,32 @@ package com.javarush.test.level19.lesson05.task03;
 12 14 1
 */
 
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader filebufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(bufferedReader.readLine())));
+        BufferedWriter fileBufferedWriter = new BufferedWriter(new FileWriter(bufferedReader.readLine()));
+
+        Pattern pattern = Pattern.compile("\\b[0-9]+\\b");
+
+        String string;
+        while ((string = filebufferedReader.readLine())!= null) {
+            Matcher matcher = pattern.matcher(string);
+            while (matcher.find()){
+                fileBufferedWriter.write(matcher.group().toString() + " ");
+                fileBufferedWriter.flush();
+            }
+        }
+
+        bufferedReader.close();
+        filebufferedReader.close();
+        fileBufferedWriter.close();
 
     }
 }
